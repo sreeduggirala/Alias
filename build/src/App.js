57ebc266b6ec5c565f0cf7ef08836c54f26d2e54
Sree Duggirala <sreevanthduggirala.@gmail.com>
2023-05-21 07:07:36 -0500

    restored front-end

---
+++ build/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header.js';
+import Body from './components/Body.js';
+import Steps from './components/Steps.js';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Body></Body>
+      <Steps></Steps>
+    </>
+  );
+}
+
+export default App;
---
+++ build/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+      <>
+      <div className="flex h-24 w-screen p-6 justify-between backdrop-blur-lg fixed top-0 sm:pl-24">
+        <h1 className="text-4xl font-bold text-orange-600">
+          Alias
+        </h1>
+        <button className="text-xl font-bold text-orange-600 border-orange-600 border-4 bg-[#f5f5f5] w-[180px] h-12 rounded-xl hover:bg-orange-600 hover:text-[#f5f5f5] duration-300">Connect Wallet</button>
+      </div>
+      </>
+    );
+}
+
+export default Header;
